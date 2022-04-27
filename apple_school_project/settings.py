c3b039f7da6505273fb2f4a8f43494f8f303b428
xenSlayer <kiranpaudel1892@gmail.com>
2022-04-27 21:24:49 +0545

    static url

--- apple_school_project/settings.py
+++ apple_school_project/settings.py
@@ -122,13 +122,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/apple/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/apple/')
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/apple/'),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'apple')
 
 # Media Files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
