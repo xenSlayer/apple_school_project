f950a6bd31078815309ed2aa9ccf88026e236d8a
xenSlayer <kiranpaudel1892@gmail.com>
2022-04-27 14:58:31 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# school-backend API
+A dynamic school management website 
+
+</br>
+
+# Features
+
+1. Blogs
+2. Events
+3. Messages notification
+4. News and announcements
+5. Testimonials
+6. API for mobile apps
+
+</br>
+
+## requirements.txt
+```
+Django==3.1.1
+djangorestframework-simplejwt==5.1.0
+django-cors-headers==3.5.0
+djangorestframework
+markdown
+django-filter
+mysqlclient==2.1.0
+Pillow
+django-enum-choices==2.1.2
+django-summernote==0.8.11.6
+django-import_export
+```
---
+++ apps/apple_school_website/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import TestimonialModel, NewsAnnouncementsModel, BlogModel, ContactUsModel, EventModel
+from django_summernote.admin import SummernoteModelAdmin
+from import_export.admin import ImportExportActionModelAdmin
+
+
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', )
+
+
+class BlogAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description', )
+
+
+admin.site.register(NewsAnnouncementsModel, NewsAdmin)
+admin.site.register(BlogModel, BlogAdmin)
+admin.site.register(TestimonialModel)
+
+
+@admin.register(EventModel)
+class ViewAdmin(ImportExportActionModelAdmin):
+    pass
+
+
+@admin.register(ContactUsModel)
+class ContactUsAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
