0d6da2be16dec144fed23fb63258e78361ac980b
xen <kiranpaudel1892@gmail.com>
2022-05-02 14:08:26 +0545

    ++

--- apple_school_project/settings.py
+++ apple_school_project/settings.py
@@ -1,6 +1,22 @@
 from apple_school_project.settings.base import *
 
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [str(BASE_DIR) + '/templates/', ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home3/kiranpau/itsmekiran.com/apple/static'
