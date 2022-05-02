8802bf244b158e3a0b18ac13dabebebb069b2f6f
xen <kiranpaudel1892@gmail.com>
2022-05-02 13:02:08 +0545

    _

--- apple_school_project/settings.py
+++ apple_school_project/settings.py
@@ -122,14 +122,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/apple/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/apple/'),
-    os.path.join('/home3/kiranpau/itsmekiran.com/apple/static/apple/'),
-    os.path.join('home3/kiranpau/itsmekiran.com/apple/static/apple/'),
-    os.path.join('/home3/kiranpau/itsmekiran.com/apple/static/apple'),
-    os.path.join('home3/kiranpau/itsmekiran.com/apple/static/apple'),
+    os.path.join(BASE_DIR, 'static/'),
+    '/home3/kiranpau/itsmekiran.com/apple/static'
 )
 
 # Media Files
