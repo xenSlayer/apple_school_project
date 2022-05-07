efa8d32f482a0f3a98b7e2b732085ff49373424e
xen <kiranpaudel1892@gmail.com>
2022-05-07 17:40:20 +0545

    host fix

--- apple_school_project/settings_.py
+++ apple_school_project/settings_.py
@@ -13,7 +13,7 @@ DATABASES = {
         'NAME': 'kiranpau_apple',
         'USER': 'kiranpau_kiran',
         'PASSWORD': f'cskWeHB7V8$yAq8GFUFQJQDx!NTrM3wme3#bX7!8sBCp5qYGcbHqzGAZC%e',
-        'HOST': 'https://apple.itsmekiran.com/',
+        'HOST': 'apple.itsmekiran.com',
         'PORT': '3306',
     }
 }
